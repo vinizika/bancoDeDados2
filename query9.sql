edd8ef30cf510e38903ebd8bff4fde78e22f29ea
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-05-15 20:32:37 -0300

    Update query9.sql

--- query9.sql
+++ query9.sql
@@ -1,4 +1,8 @@
---pegar quantidade de eventos por tipo
-SELECT tipo, COUNT(*) AS total
+--pegar quantidade de eventos por tipo de setor
+SELECT categoria.nome AS tipo, COUNT(DISTINCT evento.id_evento) AS total
 FROM evento
-GROUP BY tipo;
+JOIN ingresso ON evento.id_evento = ingresso.id_evento
+JOIN categoria ON ingresso.id_categoria = categoria.id_categoria
+GROUP BY categoria.nome;
+
+
