53887cc06f2c3d9b8181d854620edad105462604
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-05-15 20:30:00 -0300

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -2,5 +2,6 @@
 SELECT evento.nome, SUM(compra_ingresso.quantidade) AS total_ingressos
 FROM evento
 JOIN ingresso ON evento.id_evento = ingresso.id_evento
-JOIN compra_ingresso ON ingresso.id = compra_ingresso.id_ingresso
+JOIN compra_ingresso ON ingresso.id_ingresso = compra_ingresso.id_ingresso
 GROUP BY evento.nome;
+
