d8c6a6955a70c0416bfda0cd3bfdc5b9ac9e0e27
Manuella Filipe Peres <manuellafilipeperes@gmail.com>
2025-05-19 10:42:58 -0300

    Delete query10.sql

--- query10.sql
+++
@@ -1,5 +0,0 @@
---pegar quantidade de eventos que aconteceram em sp com mais de 3000 pessoas
-SELECT evento.nome
-FROM evento
-JOIN local ON evento.id_local = local.id_local
-WHERE local.endereco = 'São Paulo' AND local.capacidade > 3000;
