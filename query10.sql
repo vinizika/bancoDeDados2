f36222b53d81881cffb0c5fe229c6b3ee6c58c54
Manuella Filipe Peres <manuellafilipeperes@gmail.com>
2025-05-19 10:56:20 -0300

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -1,5 +1,5 @@
 --listar artistas que ja fizeram show mais de 2 vezes no mesmo lugar
-SELECT artista.nome
+SELECT distinct artista.nome
 FROM artista
 JOIN evento_artista ON artista.id_artista = evento_artista.id_artista
 JOIN evento ON evento_artista.id_evento = evento.id_evento
