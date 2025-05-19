8dcf165648580095165527f7706584abfd6f69af
Manuella Filipe Peres <manuellafilipeperes@gmail.com>
2025-05-19 10:42:22 -0300

    Delete query4.sql

--- query4.sql
+++
@@ -1,5 +0,0 @@
---listar eventos ocorrendo em sp
-SELECT evento.nome, local.nome, local.endereco
-FROM evento
-JOIN local ON evento.id_local = local.id_local
-WHERE local.endereco LIKE '%São Paulo%';
