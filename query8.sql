a9bc04f08804360e47d57dacb641fffcd1009c23
Manuella Filipe Peres <manuellafilipeperes@gmail.com>
2025-05-19 10:42:03 -0300

    Delete query8.sql

--- query8.sql
+++
@@ -1,7 +0,0 @@
--- pegar todos as compras que incluem mais de 2 ingressos
-SELECT compra.id_compra
-FROM compra
-JOIN compra_ingresso ON compra.id_compra = compra_ingresso.id_compra
-GROUP BY compra.id_compra
-HAVING COUNT(compra_ingresso.id_ingresso) > 2;
-
