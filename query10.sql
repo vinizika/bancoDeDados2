ad00cac41d3ad053f7b1bf3ab0e8248b73f6df6c
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-05-22 16:27:42 -0300

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -1,7 +1,11 @@
 --listar artistas que ja fizeram show mais de 1 vez no mesmo lugar
-SELECT distinct artista.nome
+SELECT 
+    artista.nome AS nome_artista,
+    local.nome AS nome_local,
+    COUNT(*) AS quantidade_eventos
 FROM artista
 JOIN evento_artista ON artista.id_artista = evento_artista.id_artista
 JOIN evento ON evento_artista.id_evento = evento.id_evento
-GROUP BY artista.nome, evento.id_local
+JOIN local ON evento.id_local = local.id_local
+GROUP BY artista.nome, local.nome
 HAVING COUNT(*) > 1;
