ed49f8e3b001f18b88d0b050ca1f84c36be58182
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-05-22 16:35:06 -0300

    Update query9.sql

--- query9.sql
+++ query9.sql
@@ -1,7 +1,9 @@
---nome dos artistas que participaram de eventos realizados em locais com capacidade superior a 100000
-SELECT DISTINCT artista.nome
+-- artistas que participaram de eventos em locais com capacidade superior a 100000, retornando o valor da maior capacidade
+SELECT DISTINCT artista.nome, MAX(local.capacidade) as maior_capacidade
 FROM artista
 JOIN evento_artista ON artista.id_artista = evento_artista.id_artista
 JOIN evento ON evento_artista.id_evento = evento.id_evento
 JOIN local ON evento.id_local = local.id_local
-WHERE local.capacidade > 100000;
+WHERE local.capacidade > 100000
+GROUP BY artista.nome
+ORDER BY MAX(local.capacidade) DESC
